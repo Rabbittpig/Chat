769d8646684b48e29c3af4d3ccd1f26e9792c35d
zwp <zhangwanping@tpson.cn>
2020-09-30 20:16:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chat/Msg.java
@@ -0,0 +1,24 @@
+package com.example.chat;
+
+/**
+ * Author: Created by zwp on 2020/9/30.
+ */
+public class Msg {
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SENT = 1;
+    private String content;
+    private int type;
+
+    public Msg(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
